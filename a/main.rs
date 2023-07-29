5a74f20dd7c48bb3e39beeae2e58c26c2dd95231
toyboot4e <toyboot4e@gmail.com>
2023-07-29 13:48:05 +0900

    change `construct_n0` signature

--- a/main.rs
+++ a/main.rs
@@ -3,7 +3,7 @@ use proconio::input;
 fn construct_n0<I, T>(
     x0: T,
     items: impl IntoIterator<Item = I, IntoIter = impl ExactSizeIterator<Item = I>>,
-    f: &impl Fn(I, &[T]) -> T,
+    f: &impl Fn(&[T], I) -> T,
 ) -> Vec<T> {
     let items = items.into_iter();
 
@@ -11,7 +11,7 @@ fn construct_n0<I, T>(
     dp.push(x0);
 
     for item in items {
-        dp.push(f(item, &dp));
+        dp.push(f(&dp, item));
     }
 
     dp
@@ -23,7 +23,7 @@ fn main() {
         hs: [isize; n],
     }
 
-    let res = construct_n0(0, &hs[1..], &|&h1, vec| {
+    let res = construct_n0(0, &hs[1..], &|vec, &h1| {
         vec.iter()
             .zip(&hs)
             .rev()
